3bf753bbf7bf3fd9b152e2624fd9c9d8064465b4
seo010204 <seo010204@naver.com>
2024-10-06 22:46:36 +0900

    Add files via upload

---
+++ C언어 5주차 과제1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char src[100];
+	char dest[100];
+	int length;
+
+	gets(src);
+	length = strlen(src);
+	printf("length of src = %d\n", length);
+	strcpy(dest, src);
+	printf("src = %s, dest = %s\n", src, dest);
+	strcat(dest, src);
+	printf("src = %s, dest = %s\n", src, dest);
+
+	return 0;
+}
